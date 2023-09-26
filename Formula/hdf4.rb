982471a7161903675d290889c4526e647bcaf71a
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2023-09-26 09:39:30 +0200

    brew formula for hdf4 v4.2.16-2

--- Formula/hdf4.rb
+++ Formula/hdf4.rb
@@ -1,28 +1,22 @@
 # frozen_string_literal: true
 
-class Nexus < Formula
-  desc "Data format library for neutron, x-ray, and muon experiments."
-  homepage "https://github.com/nexusformat/code/"
-  url "https://github.com/nexusformat/code.git",
-      tag: "v4.4.3"
-  version "4.4.3"
+class Hdf4 < Formula
+  desc "Multi-object file format library."
+  homepage "https://www.hdfgroup.org/solutions/hdf4/"
+  url "https://github.com/HDFGroup/hdf4.git",
+      tag: "hdf-4_2_16-2"
+  version "4.2.16"
 
   depends_on "cmake" => :build
   depends_on "git" => :build
-  depends_on "k61n/mlz/hdf4"
-  depends_on "hdf5"
   depends_on "jpeg"
   depends_on "zlib"
 
   def install
     cores = `sysctl -n hw.ncpu`.strip
     mkdir "build" do
-      hdf4_path = `#{HOMEBREW_PREFIX}/bin/brew --prefix hdf4`.strip
-      hdf5_path = `#{HOMEBREW_PREFIX}/bin/brew --prefix hdf5`.strip
       system "CC=/usr/bin/clang CXX=/usr/bin/clang++ " +
                "cmake .. -DCMAKE_BUILD_TYPE=Release " +
-               "-DENABLE_HDF4=1 -DHDF4_ROOT=#{hdf4_path} " +
-               "-DENABLE_HDF5=1 -DHDF5_ROOT=#{hdf5_path} " +
                "-DCMAKE_INSTALL_PREFIX=#{buildpath}/install"
       system "cmake", "--build", ".", "--parallel", cores.to_s
       system "cmake", "--install", "."
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 This tap contains following packages:
  - [musrfit v1.9.1](http://lmu.web.psi.ch/musrfit/user/html/index.html)
  - [nexus v4.4.3](https://github.com/nexusformat/code)
+ - [hdf4 v4.2.16](https://www.hdfgroup.org/solutions/hdf4)
 
 # Installation
 ```bash
