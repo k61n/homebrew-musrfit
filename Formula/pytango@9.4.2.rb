f0941a8b815906af20683ff0b57c16151c933e40
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2023-12-08 10:41:12 +0100

    pytango@9.4.2: script automatically detects libboost_python version

--- Formula/pytango@9.4.2.rb
+++ Formula/pytango@9.4.2.rb
@@ -19,13 +19,15 @@ class PytangoAT942 < Formula
     system "#{HOMEBREW_PREFIX}/bin/git apply #{repo_path}/patches/pytango"
     python_exe = "#{HOMEBREW_PREFIX}/bin/python3"
     boost_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix boost`.strip
-    boos_python_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix boost-python3`.strip
+    boost_python_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix boost-python3`.strip
     cppzmq_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix cppzmq`.strip
     omni_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix omniorb@4.2.5`.strip
     tango_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix cpptango@9.4.2`.strip
     zmq_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix zeromq`.strip
-    system "BOOST_ROOT=#{boost_root} BOOST_PYTHON_ROOT=#{boos_python_root} " +
-             "BOOST_PYTHON_LIB=boost_python311 " +
+    boost_python_deps = `#{HOMEBREW_PREFIX}/bin/brew deps boost-python3`.strip
+    python_version =  boost_python_deps.match(/python@(\d+\.\d+)/)[1].gsub(".", "")
+    system "BOOST_ROOT=#{boost_root} BOOST_PYTHON_ROOT=#{boost_python_root} " +
+             "BOOST_PYTHON_LIB=boost_python#{python_version} " +
              "CPPZMQ_ROOT=#{cppzmq_root} OMNI_ROOT=#{omni_root} " +
              "PKG_CONFIG_PATH=#{tango_root}/lib/pkgconfig TANGO_ROOT=#{tango_root} " +
              "ZMQ_ROOT=#{zmq_root} #{python_exe} setup.py build"
