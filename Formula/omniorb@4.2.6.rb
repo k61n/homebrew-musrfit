8ce986f15e1a5838b556ac4bcd0e7efbdb1c873d
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2024-06-17 15:38:50 +0200

    upd omniorb 4.2.6

--- Formula/omniorb@4.2.6.rb
+++ Formula/omniorb@4.2.6.rb
@@ -7,16 +7,17 @@ class OmniorbAT426 < Formula
   version "4.2.6"
 
   depends_on "make" => :build
-  depends_on "python" => [:build, :latest]
   depends_on "subversion" => :build
+  depends_on "python"
+  depends_on "python-setuptools"
 
   def install
     cores = `sysctl -n hw.ncpu`.strip
     mkdir "build" do
-      system "../configure PYTHON=#{HOMEBREW_PREFIX}/bin/python3 " +
+      system "../configure", "PYTHON=#{HOMEBREW_PREFIX}/bin/python3",
                "--prefix=#{buildpath}/install"
-      system "#{HOMEBREW_PREFIX}/bin/gmake -j #{cores}"
-      system "#{HOMEBREW_PREFIX}/bin/gmake install"
+      system "#{HOMEBREW_PREFIX}/bin/gmake", "-j", cores
+      system "#{HOMEBREW_PREFIX}/bin/gmake", "install"
     end
     prefix.install Dir["install/*"]
   end
