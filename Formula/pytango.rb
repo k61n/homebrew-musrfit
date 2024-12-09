d7e56d8d790bc9e5dadb9f04520fbf304dab8402
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2024-12-09 16:38:28 +0100

    pytango v10.0.0
    builds from source
    only for python3.12

---
+++ Formula/pytango.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class Pytango < Formula
+  desc "Python module for cppTango C++ libraries"
+  homepage "https://www.tango-controls.org/"
+  url "https://gitlab.com/tango-controls/pytango.git",
+      tag: "v10.0.0"
+  version "10.0.0"
+
+  depends_on "cmake" => :build
+  depends_on "git" => :build
+  depends_on "ninja" => :build
+  depends_on "boost"
+  depends_on "boost-python3"
+  depends_on "numpy"
+  depends_on "python@3.12"
+  depends_on "mlz/packages/cpptango"
+
+  def install
+    python_exe = "#{HOMEBREW_PREFIX}/bin/python3.12"
+    ENV['BOOST_ROOT'] = `#{HOMEBREW_PREFIX}/bin/brew --prefix boost`.strip
+    ENV['BOOST_PYTHON_SUFFIX'] = `#{HOMEBREW_PREFIX}/bin/brew deps boost-python3`.strip.match(/python@(\d+\.\d+)/)[1].gsub(".", "")
+    ENV['CPPZMQ_ROOT'] = `#{HOMEBREW_PREFIX}/bin/brew --prefix cppzmq`.strip
+    ENV['omniORB4_ROOT'] = `#{HOMEBREW_PREFIX}/bin/brew --prefix omniorb`.strip
+    ENV['Tango_ROOT'] = `#{HOMEBREW_PREFIX}/bin/brew --prefix cpptango`.strip
+    system python_exe, "-m", "pip", "install", *std_pip_args(build_isolation: true), "."
+  end
+
+  test do
+    python_exe = "#{HOMEBREW_PREFIX}/bin/python3.12"
+    system python_exe, "-c", "from tango import DeviceProxy"
+  end
+end
