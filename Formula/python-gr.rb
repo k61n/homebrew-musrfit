fd5f8e81d2209f1a2ec1ce354e48b03fedcfb78a
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2024-12-09 11:50:46 +0100

    python-gr v1.25.0
    also installed for each python3 version in brew

--- Formula/python-gr.rb
+++ Formula/python-gr.rb
@@ -4,22 +4,34 @@ class PythonGr < Formula
   desc "GR - a universal framework for visualization applications"
   homepage "https://gr-framework.org/"
   url "https://github.com/sciapp/python-gr.git",
-      tag: "v1.24.0"
-  version "1.24.0"
+      tag: "v1.25.0"
+  version "1.25.0"
 
   depends_on "mlz/packages/gr"
   depends_on "mlz/packages/python-vcversioner"
   depends_on "numpy"
-  depends_on "python@3.12"
+  depends_on "python"
 
   def install
-    python_exe = "#{HOMEBREW_PREFIX}/bin/python3.12"
-    gr_root = `#{HOMEBREW_PREFIX}/bin/brew --prefix gr`.strip
-    ENV['GRLIB'] = "#{gr_root}/lib"
-    system python_exe, "-m", "pip", "install", *std_pip_args(build_isolation: true), "."
+    ENV['GRLIB'] = `#{HOMEBREW_PREFIX}/bin/brew --prefix gr`.strip
+    pythons = `#{HOMEBREW_PREFIX}/bin/brew list | grep python@`.strip.split("\n")
+    pythons.each do |python|
+      # can't install for python3.9
+      if python.gsub("python@3.", "").to_i > 9
+        python_exe = "#{HOMEBREW_PREFIX}/bin/#{python.gsub("@", "")}"
+        system python_exe, "-m", "pip", "install", *std_pip_args(build_isolation: true), "."
+      end
+    end
   end
 
   test do
-    # Test commands to verify that your software is working
+    pythons = `#{HOMEBREW_PREFIX}/bin/brew list | grep python@`.strip.split("\n")
+    pythons.each do |python|
+      # no numpy for pythons < 3.12
+      if python.gsub("python@3.", "").to_i > 11
+        python_exe = "#{HOMEBREW_PREFIX}/bin/#{python.gsub("@", "")}"
+        system python_exe, "-c", "from gr import pygr"
+      end
+    end
   end
 end
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ This tap contains following packages:
  - nicos-pyctl [v1.3.0](https://github.com/mlz-ictrl/nicos-pyctl) - [homepage](https://github.com/mlz-ictrl/nicos-pyctl)
  - numpy [v1.26.4](https://github.com/numpy/numpy) - [homepage](https://numpy.org/)
  - pytango [v9.5.1](https://gitlab.com/tango-controls/pytango) - [homepage](https://www.tango-controls.org)
- - python-gr [v1.24.0](https://github.com/sciapp/python-gr) - [homepage](https://gr-framework.org)
+ - python-gr [v1.25.0](https://github.com/sciapp/python-gr) - [homepage](https://gr-framework.org)
  - python-psutil [v5.9.8](https://github.com/giampaolo/psutil) - [homepage](https://github.com/giampaolo/psutil)
  - python-rsa [v4.9](https://github.com/sybrenstuvel/python-rsa) - [homepage](https://stuvel.eu/rsa)
  - python-toml [v0.10.2](https://github.com/uiri/toml) - [homepage](https://github.com/uiri/toml)
