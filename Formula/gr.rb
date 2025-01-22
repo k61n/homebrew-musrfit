773220827cfdabae3013b0ce4732e46faaad9665
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2025-01-22 18:06:16 +0100

    gr v0.73.12

--- Formula/gr.rb
+++ Formula/gr.rb
@@ -4,8 +4,8 @@ class Gr < Formula
   desc "GR - a universal framework for visualization applications"
   homepage "https://gr-framework.org/"
   url "https://github.com/sciapp/gr.git",
-      tag: "v0.73.10"
-  version "0.73.10"
+      tag: "v0.73.12"
+  version "0.73.12"
 
   depends_on "cmake" => :build
   depends_on "git" => :build
